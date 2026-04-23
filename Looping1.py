b5a1f1ddd7fc2c91473c60d6cceb7d7d2d3bbcd9
prabuoktoanmas-cyber <prabuoktoanmas@gmail.com>
2026-04-23 12:34:40 +0700

    Add nested loop with conditional printing

---
+++ Looping1.py
@@ -0,0 +1,9 @@
+for i in range(5) :
+    for x in range(5) :
+        if i == 2 or i == 2:
+            print("*", end=" ")
+        elif x == 2 or x == 2:
+            print("*", end=" ")
+        else:
+            print("-", end=" ")      
+    print()
